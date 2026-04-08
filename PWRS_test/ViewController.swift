77d6037a7efbda96a35e4d0ebe0f7336c7cd7e3f
Aleksey Chaykin <barjastroy@gmail.com>
2026-04-08 09:50:48 +0300

    Initial Commit

---
+++ PWRS_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PWRS_test
+//
+//  Created by A Ch on 08.04.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
